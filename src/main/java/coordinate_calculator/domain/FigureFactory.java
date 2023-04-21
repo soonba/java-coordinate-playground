030cac713e7ab767ae78a2e198c18fe2cb581f44
sunba <sunba2318@gmail.com>
2023-04-21 18:58:47 +0900

    test Square validation 테스트 코드 작성

--- src/main/java/coordinate_calculator/domain/FigureFactory.java
+++ src/main/java/coordinate_calculator/domain/FigureFactory.java
@@ -5,11 +5,12 @@ import java.security.InvalidParameterException;
 public class FigureFactory {
 
     public static final int SIZE_LINE = 2;
+    private static final int SIZE_SQUARE = 4;
 
     public static Figure getInstance(Points points) {
         int size = points.getSize();
         if(size == SIZE_LINE) return new Line(points);
-//        if(size == SIZE_SQUARE) return new SQURE(points);
+        if(size == SIZE_SQUARE) return new Square(points);
         throw new InvalidParameterException("dd");
     }
 }
